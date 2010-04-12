f65e27bf188b1482d0d6217efac704f4996f26ba
Aredridel <aredridel@nbtsc.org>
2010-04-12 09:56:03 +0200

    - add fake DOM

--- html5/treebuilder.js
+++ html5/treebuilder.js
@@ -1,7 +1,44 @@
+var sys = require('sys');
 exports.TreeBuilder = b = function TreeBuilder() {
-
+	this.open_elements = [];
+	this.document = new FakeDomDocument();
 }
 
 b.prototype.reset = function() {
 
+}
+
+b.prototype.createElement = function (name, attributes) {
+	sys.puts("createElement " + name + " with attributes " + sys.inspect(attributes));
+	return new FakeDomElement(name, attributes);
+}
+
+b.prototype.insert_element = function(name, attributes, namespace) {
+	var element = new FakeDomElement(name, namespace);
+	element.attributes = attributes;
+	this.open_elements[this.open_elements.length - 1].appendChild(element);
+	this.open_elements.push(element);
+	return element;
+}
+
+function FakeDomDocument() {
+	this.child = null;
+}
+
+FakeDomDocument.prototype = {
+	appendChild: function(element)  {
+		if(this.child) throw('Already have a root element');
+		this.child = element;
+	}
+}
+
+function FakeDomElement(name) {
+	this.tagName = name;
+	this.children = [];
+}
+
+FakeDomElement.prototype = {
+	appendChild: function(element) {	
+		this.children.push(element);
+	}
 }
