2799f61650655b5c2e013811fda119d34351f489
Aredridel <aredridel@nbtsc.org>
2010-04-15 08:54:05 +0200

    - print out result in test

--- tests/test_html5parser.js
+++ tests/test_html5parser.js
@@ -1,3 +1,7 @@
 var Parser = require('html5/parser').Parser;
 
 p = new Parser("<html><head><title>Hello!</title></head><body><p>Hi!</p></body></html>");
+
+var sys = require('sys');
+
+sys.puts(sys.inspect(p.tree, false, null));
