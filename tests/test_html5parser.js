28a26d12ebab2cd3ca8b157e460a768ffcb21313
Aredridel <aredridel@nbtsc.org>
2010-04-15 06:09:42 +0200

    - move parser into html5/parser.js

--- tests/test_html5parser.js
+++ tests/test_html5parser.js
@@ -1,3 +1,3 @@
-var Parser = require('html5parser').Parser;
+var Parser = require('html5/parser').Parser;
 
 p = new Parser("<html><head><title>Hello!</title></head><body><p>Hi!</p></body></html>");
