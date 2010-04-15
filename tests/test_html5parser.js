d9162f7ccf4bbbac75c3a758789069806456c145
Aria Stewart <aredridel@nbtsc.org>
2010-04-15 13:54:40 -0600

    - check error counts

--- tests/test_html5parser.js
+++ tests/test_html5parser.js
@@ -3,19 +3,34 @@ var assert = require('assert');
 var sys = require('sys');
 
 var data = {
-	trivial: "<html><head><title>Hello!</title></head><body><p>Hi!</p><div>Testing</div></body></html>",
-	attr: "<html><head profile='x'><title>Hello!</title></head><body class='test'></body></html>",
-	minimal: "<p>Hi!</p>",
+	trivial: {
+		code: "<html><head><title>Hello!</title></head><body><p>Hi!</p><div>Testing</div></body></html>",
+		errorCount: 1
+	},
+	attr: {
+		code: "<html><head profile='x'><title>Hello!</title></head><body class='test'></body></html>",
+		errorCount: 1
+	},
+	minimal: {
+		code: "<p>Hi!</p>",
+		errorCount: 1
+	},
+	unfinished: {
+		code: "<html><head><title>Well then",
+	},
 };
 
-function basic_parser_checks(p) {
+function basic_parser_checks(p, d) {
 	assert.ok(p);
 	assert.ok(p.tree);
 	assert.ok(p.tree.document);
+	assert.ok(p.tree.document.root);
+	if(d.errorCount) assert.equal(p.errors.length, d.errorCount);
 }
 
 for(i in data) {
-	p = new HTML5.Parser(data[i]);
-	basic_parser_checks(p);
+	p = new HTML5.Parser(data[i].code);
+	basic_parser_checks(p, data[i]);
 	sys.puts(sys.inspect(p.tree.document, false, null));
+	sys.puts(sys.inspect(p.errors, false, null));
 }
