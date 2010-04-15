a1b769ebb0687e80cd89913a61c05fc6671952bc
Aria Stewart <aredridel@nbtsc.org>
2010-04-15 13:44:37 -0600

    - start using a battery of tests

--- tests/test_html5parser.js
+++ tests/test_html5parser.js
@@ -1,7 +1,21 @@
 var HTML5 = require('html5/parser').HTML5;
-
-p = new HTML5.Parser("<html><head><title>Hello!</title></head><body><p>Hi!</p><div>Testing</div></body></html>");
-
+var assert = require('assert');
 var sys = require('sys');
 
-sys.puts(sys.inspect(p.tree, false, null));
+var data = {
+	trivial: "<html><head><title>Hello!</title></head><body><p>Hi!</p><div>Testing</div></body></html>",
+	attr: "<html><head profile='x'><title>Hello!</title></head><body class='test'></body></html>",
+	minimal: "<p>Hi!</p>",
+};
+
+function basic_parser_checks(p) {
+	assert.ok(p);
+	assert.ok(p.tree);
+	assert.ok(p.tree.document);
+}
+
+for(i in data) {
+	p = new HTML5.Parser(data[i]);
+	basic_parser_checks(p);
+	sys.puts(sys.inspect(p.tree.document, false, null));
+}
