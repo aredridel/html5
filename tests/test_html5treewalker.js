80a9c51c8d8cda840ed3893fd378dd7fa2f51cb6
Aria Stewart <aredridel@nbtsc.org>
2010-04-23 10:23:05 -0600

    - add treewalker

---
+++ tests/test_html5treewalker.js
@@ -0,0 +1,8 @@
+var HTML5 = require('html5');
+var sys = require('sys');
+
+var p = new HTML5.Parser('<p>Hi!</p>');
+
+var w = new HTML5.TreeWalker(p.tree, function(token) {
+	sys.puts(sys.inspect(token));
+});
