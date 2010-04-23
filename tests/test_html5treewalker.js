9486b70cd2f403c58aa89dd6db7019d911c11c7a
Aria Stewart <aredridel@nbtsc.org>
2010-04-23 16:11:44 -0600

    - clean up treewalker - add serializer to html5 module

--- tests/test_html5treewalker.js
+++ tests/test_html5treewalker.js
@@ -3,6 +3,6 @@ var sys = require('sys');
 
 var p = new HTML5.Parser('<p>Hi!</p>');
 
-var w = new HTML5.TreeWalker(p.tree, function(token) {
+var w = new HTML5.TreeWalker(p.tree.document, function(token) {
 	sys.puts(sys.inspect(token));
 });
