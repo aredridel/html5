de884aae9d402f4c68e3ada26ef3a0e61331533f
Aria Stewart <aredridel@nbtsc.org>
2010-04-28 22:30:47 -0600

    - add channelized debug statements

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 require.paths.push('lib')
 require.paths.push('.')
 
+var HTML5 = require('html5')
+HTML5.enableDebug('tokenizer');
+HTML5.enableDebug('parser');
+
 var testrunner = require('testrunner');
 testrunner.run(['tests'])
