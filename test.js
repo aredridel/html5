19e53a18e286d0d42e537cc939f063839e4525dd
Aria Stewart <aredridel@nbtsc.org>
2010-04-29 00:15:28 -0600

    - add debugging

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ require.paths.push('.')
 var HTML5 = require('html5')
 HTML5.enableDebug('tokenizer');
 HTML5.enableDebug('parser');
+HTML5.enableDebug('testdata');
 
 var testrunner = require('testrunner');
 testrunner.run(['tests'])
