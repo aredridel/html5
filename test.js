0c1efe36d9d0709a8bd1232afd2a59e03a1db604
Aria Stewart <aredridel@nbtsc.org>
2010-05-01 23:32:07 -0600

    - remove debug from testsuite, since testbed lets us be more specific and focused

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 require.paths.push('lib')
 require.paths.push('.')
 
-var HTML5 = require('html5')
-HTML5.enableDebug('tokenizer');
-HTML5.enableDebug('parser');
-HTML5.enableDebug('testdata');
-
 var testrunner = require('testrunner');
 testrunner.run(['tests'])
