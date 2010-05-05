113158da03740e73917c17acfaa6469435e23831
Aria Stewart <aredridel@nbtsc.org>
2010-05-05 00:28:10 -0600

    - fix attribute handling

--- tests/support/serializeTestOutput.js
+++ tests/support/serializeTestOutput.js
@@ -1,5 +1,6 @@
 var dom = require('dom');
-var walker = require('html5').HTML5.TreeWalker;
+var HTML5 = require('html5');
+var walker = HTML5.TreeWalker;
 
 exports.serializeTestOutput = function(doc) {
 	var s = '';
@@ -8,7 +9,11 @@ exports.serializeTestOutput = function(doc) {
 		switch(token.type) {
 		case 'StartTag':
 			s += indent + '<' + token.name + ">\n";
+HTML5.debug('serialize', token)
 			indent += '  ';
+			for(var i in token.data) {
+				s += indent + i + '="' + token.data[i] + '"\n'
+			}
 			break;
 		case 'EmptyTag':
 			s += indent + '<' + token.name + '>\n';
