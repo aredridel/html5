5299bfeba441a4eb816c33e5a5238beb09b06439
Aria Stewart <aredridel@nbtsc.org>
2010-05-26 08:54:14 -0600

    - lowercase during serialization by default, and add option to change that

--- tests/serializer.js
+++ tests/serializer.js
@@ -2,7 +2,7 @@
 exports.testSerializer = function(test) {
 	test.expect(1);
 	var HTML5 = require('html5');
-	var p = new HTML5.Parser('<p>Hi!</p>');
-	test.equals(HTML5.serialize(p.tree.document), '<html><head></head><body><p>Hi!</p></body></html>');
+	var p = new HTML5.Parser('<p class="Hello">Hi!</p><p class="hello there">Hi again</p>');
+	test.equals(HTML5.serialize(p.tree.document, null, {lowercase: true}), '<html><head></head><body><p class=Hello>Hi!</p><p class="hello there">Hi again</p></body></html>');
 	test.done();
 }
