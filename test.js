95b2855a9cd74914ea429251da0f6714d1b3460c
Aria Stewart <aredridel@nbtsc.org>
2010-09-12 08:17:28 -0600

    Allow more selective test running if desired

--- test.js
+++ test.js
@@ -3,4 +3,9 @@ require.paths.push('.')
 require.paths.push('deps/jsdom/lib')
 
 var testrunner = require('testrunner');
-testrunner.run(['tests'])
+var args = process.ARGV.slice(2);
+if(args.length > 0) {
+	testrunner.run(args);
+} else {
+	testrunner.run(['tests'])
+}
