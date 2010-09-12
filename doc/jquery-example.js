76097769443b9811473b6c90993c15b061e6348a
Aria Stewart <aredridel@nbtsc.org>
2010-09-12 00:14:43 -0600

    add jquery example

---
+++ doc/jquery-example.js
@@ -0,0 +1,20 @@
+require.paths.unshift('lib')
+require.paths.unshift('deps/jsdom/lib')
+var  HTML5 = require('html5'),
+       DOM = require('jsdom/level1/core').dom.level1.core,
+    Script = process.binding('evals').Script,
+       sys = require('sys'),
+        fs = require('fs'),
+     jsdom = require('jsdom'),
+    window = jsdom.createWindow(null, null, {parser: HTML5});
+
+var parser = new HTML5.Parser({document: window.document});
+
+var inputfile = fs.readFileSync('doc/jquery-example.html');
+parser.parse(inputfile);
+
+jsdom.jQueryify(window, 'deps/jquery/dist/jquery.js', function(window, jquery) {
+	Script.runInNewContext('jQuery("p").append("<b>Hi!</b>")', window);
+	sys.puts(window.document.innerHTML);
+
+});
