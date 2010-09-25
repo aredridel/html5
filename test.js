184a4268c8f7552e413e90bfa0a01ce83b2869aa
Aria Stewart <aredridel@linux-4g3u.site>
2010-09-25 10:16:19 -0600

    Add nodeunit as a submodule

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 require.paths.push('lib')
 require.paths.push('.')
 require.paths.push('deps/jsdom/lib')
+require.paths.push('deps/nodeunit/lib')
 
 var testrunner = require('testrunner');
 var args = process.ARGV.slice(2);
