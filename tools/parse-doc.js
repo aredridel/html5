e553b558de58222a44b1c40417b383c4d66ebdd8
Aria Stewart <aredridel@nbtsc.org>
2010-10-12 20:15:10 -0600

    Add parse-doc.js, to use to parse a file of HTML

---
+++ tools/parse-doc.js
@@ -0,0 +1,23 @@
+require.paths.push('lib');
+require.paths.push('deps/jsdom/lib');
+var HTML5 = require('html5'),
+	events = require('events'),
+	sys = require('sys'),
+	fs = require('fs'),
+	assert = require('assert')
+
+var inp = fs.createReadStream(process.argv[2])
+
+var p = new HTML5.Parser();
+
+if(process.argv[3]) {
+	var debugs = process.argv[3].split(',');
+	for(var d in debugs) HTML5.enableDebug(debugs[d]);
+}
+
+p.on('end', function() {
+	console.log(p.document.innerHTML)
+})
+
+p.parse(inp);
+
