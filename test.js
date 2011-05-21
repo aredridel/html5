cd7b7a42dd094be5903c58a93d24f6cceb535c51
Aria Stewart <aredridel@nbtsc.org>
2011-05-20 22:40:54 -0600

    Rely on npm to set up dependencies

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 require.paths.unshift('lib')
 require.paths.unshift('.')
-require.paths.unshift('deps/jsdom/lib')
-require.paths.unshift('deps/nodeunit/lib')
 
 var reporter = require('nodeunit').reporters.default;
 var args = process.ARGV.slice(2);
