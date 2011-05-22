4907139d6a7d5a06bc20aa42e6231952cce43e41
Aria Stewart <aredridel@nbtsc.org>
2011-05-22 00:00:52 -0600

    Convert serializer test to vows

--- test/serializer.js
+++ test/serializer.js
@@ -1,9 +1,16 @@
+var vows = require('vows')
+var assert = require('assert')
+var HTML5 = require('../lib/html5')
 
-exports.testSerializer = function(test) {
-	test.expect(1);
-	var HTML5 = require('html5');
-	var p = new HTML5.Parser();
-	p.parse('<p class="Hello">Hi!</p><p class="hello there">Hi again</p>');
-	test.equals(HTML5.serialize(p.tree.document, null, {lowercase: true}), '<html><head></head><body><p class=Hello>Hi!</p><p class="hello there">Hi again</p></body></html>');
-	test.done();
-}
+vows.describe('serializer').addBatch({
+	'Test Serializer': {
+		topic: function() {
+			var p = new HTML5.Parser()
+			p.parse('<p class="Hello">Hi!</p><p class="hello there">Hi again</p>')
+			this.callback(null, p)
+		},
+		'should serialize properly': function(p) {
+			assert.equal(HTML5.serialize(p.tree.document, null, {lowercase: true}), '<html><head></head><body><p class="Hello">Hi!</p><p class="hello there">Hi again</p></body></html>');
+		}
+	}
+}).export(module)
