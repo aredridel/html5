b4b481d26c947e933897b5fb16528e82d40aaabf
Aria Stewart <aredridel@nbtsc.org>
2011-11-12 23:40:14 -0500

    Fix parse-test-data script

--- tools/parse-test-data.js
+++ tools/parse-test-data.js
@@ -1,12 +1,12 @@
-require.paths.push('lib')
 var fs= require('fs')
 
 var doc = fs.readFileSync(process.argv[2])
 
-var HTML5 = require('html5')
-//HTML5.enableDebug('tokenizer')
-//HTML5.enableDebug('parser')
+var HTML5 = require('../lib/html5')
+HTML5.enableDebug('tokenizer')
+HTML5.enableDebug('parser')
 
-var p = new HTML5.Parser(doc) 
+var p = new HTML5.Parser() 
+    p.parse(doc)
 
-require('sys').puts(HTML5.serialize(p.tree.document))
+require('util').puts(HTML5.serialize(p.tree.document))
