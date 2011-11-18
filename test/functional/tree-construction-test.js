5d2bb8fce99d6e07f2f60ae0e535ea4c9a366d72
Aria Stewart <aredridel@nbtsc.org>
2011-11-17 22:14:46 -0500

    Make skip-to-test code more obvious

--- test/functional/tree-construction-test.js
+++ test/functional/tree-construction-test.js
@@ -7,6 +7,21 @@ var HTML5 = require('../../lib/html5'),
 var support = require('../lib/readTestData')
 var base = __dirname + '/../../data/tree-construction/'
 var testList = fs.readdirSync(base);
+
+if(typeof process.argv[2] != 'undefined') {
+    var logdata = true
+    var skipToTest = process.argv[2]
+} else {
+    var logdata = false
+    var skipToTest = false
+}
+
+if(typeof process.argv[3] != 'undefined') {
+    var skipToTestNumber = process.argv[3]
+} else {
+    var skipToTestNumber = false
+}
+
 for(var i in testList) {
 	var testname = testList[i];
 
@@ -18,9 +33,17 @@ for(var i in testList) {
 
 	for(var i in testData) {
         (function(td) {
+            if(skipToTest) {
+                if(skipToTest != testname) return;
+                if(skipToTestNumber && skipToTestNumber != i) return;
+            }
             test(testname+':'+i, function(t) {
                 try {
                     var p = new HTML5.Parser()
+                    if(logdata) {
+                        console.log("Input: " + td.data)
+                        console.log("Expected: " + td.document)
+                    }
                     if(td['document-fragment']) {
                         p.parse_fragment(td.data.slice(0, td.data.length - 1), td['document-fragment'].trimRight())
                     } else {
