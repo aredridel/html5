0e4bdbf779a0fa00288b96d5e934dace1b258af1
Aria Stewart <aredridel@nbtsc.org>
2012-04-23 23:02:13 -0400

    Split test data into separate files; add support for TODO tests

--- test/functional/tree-construction-test.js
+++ test/functional/tree-construction-test.js
@@ -1,65 +1,57 @@
+"use strict";
 var HTML5 = require('../../lib/html5'),
 	events = require('events'),
 	fs = require('fs'),
+	async = require('async'),
 	test = require('tap').test,
 	serialize = require('../lib/serializeTestOutput').serializeTestOutput;
 
-var support = require('../lib/readTestData')
 var base = __dirname + '/../../data/tree-construction/'
 var testList = fs.readdirSync(base);
 
 if(typeof process.argv[2] != 'undefined') {
-    var logdata = true
-    var skipToTest = process.argv[2]
-} else {
-    var logdata = false
-    var skipToTest = false
-}
-
-if(typeof process.argv[3] != 'undefined') {
-    var skipToTestNumber = process.argv[3]
-} else {
-    var skipToTestNumber = false
-}
-
-if(typeof process.argv[4] != 'undefined') {
-    var debugs = process.argv[4].split(',')
+    var debugs = process.argv[2].split(',')
     for(var i in debugs) {
         HTML5.enableDebug(debugs[i])
     }
 }
 
+function doTest(testName) {
+	test(testName, function(t) {
+		var todo = false;
+		var testData = {};
+		try {
+			testData = JSON.parse(fs.readFileSync(testName+'/info.json'));
+		} catch(e) {
+			if(e.code !== 'ENOENT') throw e;
+		}
+
+		try {
+			todo = JSON.parse(fs.readFileSync(testName + '/todo.json'));
+		} catch (e) {
+			if(e.code !== 'ENOENT') throw e;
+		}
+
+		var input = fs.readFileSync(testName+'/input.html', 'utf-8');
+		var document = fs.readFileSync(testName+'/result.tree', 'utf-8');
+
+		var p = new HTML5.Parser()
+		if(testData['document-fragment']) {
+			p.parse_fragment(input.slice(0, input.length - 1), testData['document-fragment'].trimRight())
+		} else {
+			p.parse(input.slice(0, input.length - 1));
+		}
+		var serialized = serialize(p.inner_html ? p.tree.getFragment() : p.tree.document);
+		t.equal(serialized, document, (todo ? "TODO " : "") + "Document matches example data")
+		t.end()
+	})
+}
+
 for(var i in testList) {
 	var testname = testList[i];
 
-	if(testname.match(/\.js$/)) continue;
-
-	if(fs.statSync(base+testname).isDirectory()) continue;
-
-	var testData = support.readTestData(base+testname);
-
-	for(var i in testData) {
-        (function(td) {
-            if(skipToTest) {
-                if(skipToTest != testname) return;
-                if(skipToTestNumber && skipToTestNumber != i) return;
-            }
-            test(testname+':'+i, function(t) {
-                var p = new HTML5.Parser()
-                if(logdata) {
-                    console.log("Input: " + td.data)
-                    console.log("Expected: " + td.document)
-                }
-                if(td['document-fragment']) {
-                    p.parse_fragment(td.data.slice(0, td.data.length - 1), td['document-fragment'].trimRight())
-                } else {
-                    p.parse(td.data.slice(0, td.data.length - 1));
-                }
-                var serialized = serialize(p.inner_html ? p.tree.getFragment() : p.tree.document);
-                t.equal(serialized, td.document, "Document matches example data")
-                t.ok("No exception");
-                t.end()
-            })
-		})(testData[i])
+	if(fs.statSync(base+testname).isDirectory() && fs.statSync(base+testname+'/input.html')) {
+		doTest(base+testname);
 	}
+
 }
