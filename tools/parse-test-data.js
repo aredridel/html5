9888708c9a3e96cec611f2d5051b0f0f55e7b47b
Aria Stewart <aredridel@nbtsc.org>
2012-11-11 21:31:43 -0500

    Add shebang to tools/parse-test-data.js

--- tools/parse-test-data.js
+++ tools/parse-test-data.js
@@ -1,12 +1,13 @@
-var fs= require('fs')
+#!/usr/bin/env node
+var fs = require('fs');
 
-var doc = fs.readFileSync(process.argv[2])
+var doc = fs.readFileSync(process.argv[2]);
 
-var HTML5 = require('../lib/html5')
-HTML5.enableDebug('tokenizer')
-HTML5.enableDebug('parser')
+var HTML5 = require('../lib/html5');
+HTML5.enableDebug('tokenizer');
+HTML5.enableDebug('parser');
 
-var p = new HTML5.Parser() 
-    p.parse(doc)
+var p = new HTML5.Parser();
+p.parse(doc);
 
-require('util').puts(HTML5.serialize(p.tree.document))
+require('util').puts(HTML5.serialize(p.tree.document));
