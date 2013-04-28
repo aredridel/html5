da9ba3ab822601a3915b5a4ba156a8e49ba08ad3
Aria Stewart <aredridel@nbtsc.org>
2013-04-27 21:17:39 -0400

    whitespace

--- test/functional/tree-construction-test.js
+++ test/functional/tree-construction-test.js
@@ -9,27 +9,27 @@ var HTML5 = require('../../lib/html5'),
 var base = __dirname + '/../../data/tree-construction/'
 var testList = fs.readdirSync(base);
 
-if(typeof process.argv[2] != 'undefined') {
+if (typeof process.argv[2] != 'undefined') {
     var debugs = process.argv[2].split(',')
-    for(var i in debugs) {
+    for (var i in debugs) {
         HTML5.enableDebug(debugs[i])
     }
 }
 
 function doTest(testName) {
-	test(testName, function(t) {
+	test(testName, function (t) {
 		var todo = false;
 		var testData = {};
 		try {
 			testData = JSON.parse(fs.readFileSync(testName+'/info.json'));
 		} catch(e) {
-			if(e.code !== 'ENOENT') throw e;
+			if (e.code !== 'ENOENT') throw e;
 		}
 
 		try {
 			todo = JSON.parse(fs.readFileSync(testName + '/todo.json'));
 		} catch (e) {
-			if(e.code !== 'ENOENT') throw e;
+			if (e.code !== 'ENOENT') throw e;
 		}
 
 		var input = fs.readFileSync(testName+'/input.html', 'utf-8');
@@ -37,7 +37,7 @@ function doTest(testName) {
 
 		try {
 			var p = new HTML5.Parser()
-			if(testData['document-fragment']) {
+			if (testData['document-fragment']) {
 				p.parse_fragment(input.slice(0, input.length - 1), testData['document-fragment'].trimRight())
 			} else {
 				p.parse(input.slice(0, input.length - 1));
@@ -51,11 +51,10 @@ function doTest(testName) {
 	})
 }
 
-for(var i in testList) {
+for (var i in testList) {
 	var testname = testList[i];
 
-	if(fs.statSync(base+testname).isDirectory() && fs.statSync(base+testname+'/input.html')) {
+	if (fs.statSync(base+testname).isDirectory() && fs.statSync(base+testname+'/input.html')) {
 		doTest(base+testname);
 	}
-
 }
